7389f0f06b81fe7fe562dd74a654b8c375c07a21
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2022-09-26 16:35:14 +0300

    Перше та друге завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryBoxRef = document.querySelector('.gallery');
+
+//створення картинок
+function creatingPictureMarkup(gallery) {
+  return gallery
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join('');
+}
+
+galleryBoxRef.insertAdjacentHTML(
+  'beforeend',
+  creatingPictureMarkup(galleryItems)
+);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const URRENT_POSITION_PLAYBACK = 'videoplayer-current-time';
 
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const player = new Player(iframe);
+const throttleSaveCurrentTime = throttle(saveCurrentTime, 1000);
+
+player.on('timeupdate', throttleSaveCurrentTime);
+
+function saveCurrentTime(data) {
+  const videoStopTime = data.seconds;
+  save(URRENT_POSITION_PLAYBACK, videoStopTime);
+}
+
+player.setCurrentTime(load(URRENT_POSITION_PLAYBACK));
